1ae4d1eb68e6ec2b9818db7df5f3da162c70aaae
PremSathe <premsathe78@gmail.com>
2024-06-27 15:25:17 +0530

    tic-tac

--- src/TicTacToe/TicTacToe.jsx
+++ src/TicTacToe/TicTacToe.jsx
@@ -6,27 +6,29 @@ import cross_icon from "../Assets/cross.png";
 const TicTacToe = () => {
   return (
     <div className="container">
-      <div className="title">
+      <h1 className="title">
+        {" "}
         Tic Tac Toe Game in <span>React</span>
-        <div className="board">
-          <div className="row-1">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-          </div>
-          <div className="row-2">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-          </div>
-          <div className="row-3">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-          </div>
+      </h1>
+      <div className="board" style={{ marginTop: 50 }}>
+        {" "}
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
         </div>
       </div>
-      <button className="btn">Reset</button>
+      <button className="reset">Reset</button>
     </div>
   );
 };
